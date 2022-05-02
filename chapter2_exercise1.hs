d289e9ebdfb9b05f096d531955dc06c4bca98243
Frank Albanese <faalbane@gmail.com>
2022-05-02 09:23:23 -0600

    add instruction comment

--- chapter2_exercise1.hs
+++ chapter2_exercise1.hs
@@ -1,3 +1,11 @@
+{-
+(2) Fix the syntax errors in the program below, and test your solution using GHCi. 
+
+N = a ’div’ length xs where
+       a = 10
+      xs = [1,2,3,4,5] 
+-}
+
 n = a `div` length xs 
     where
         a = 10
