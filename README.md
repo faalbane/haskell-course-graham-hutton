985fefed7abbe85c821b13f502446eec97d3cab2
Frank Albanese <faalbane@gmail.com>
2022-05-02 07:50:31 -0600

    add standard_library_functions.hs

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # haskell-course-graham-hutton
 
-Step 1. Install GHC 
+# Step 1. Install GHC 
 
 brew install ghc 
+
+=-=-=
+
+# Starting GHC (general; no specific file): 
+
+ghci 
+
+# Exiting GHC (general; no specific file): 
+
+:q 
+
+# Starting GHC (specific file): 
+
+ghci <filename> 
+
+# Exiting GCH (specific file): 
+
+ctrl+d 
 
---
+++ standard_library_functions.hs
@@ -0,0 +1,43 @@
+import Data.List
+
+-- | head 
+-- | head [1,2,3,4,5]
+-- | 1
+
+-- | tail 
+-- | tail [1,2,3,4,5]
+-- | [2,3,4,5] 
+
+-- | select the nth element of a list 
+-- | [1,2,3,4,5] !! 2 
+-- | 3 
+
+-- | select the first n elements of a list 
+-- | take 3 [1,2,3,4,5] 
+-- | [1,2,3] 
+
+-- | remove the first n elements from a list 
+-- | drop 3 [1,2,3,4,5] 
+-- | [4,5] 
+
+-- | calculate the length of a list 
+-- | length [1,2,3,4,5] 
+-- | 5 
+
+-- | calculate the sum of a list of numbers 
+-- | sum [1,2,3,4,5] 
+-- | 15 
+
+-- | calculate the product of a list of numbers 
+-- | product [1,2,3,4,5] 
+-- | 120 
+
+-- | append two lists 
+-- | [1,2,3] ++ [4,5] 
+-- | [1,2,3,4,5] 
+
+-- | reverse a list 
+-- | reverse [1,2,3,4,5] 
+-- | [5,4,3,2,1] 
+
+-- | 
